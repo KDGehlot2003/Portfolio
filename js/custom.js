3537fa8a80334d24e1c313422a8d9a2a3e630a70
Kshitij Gehlot <kdgehlot2003@gmail.com>
2024-05-28 19:51:54 +0530

    minor changes

--- js/custom.js
+++ js/custom.js
@@ -104,7 +104,7 @@ $(document).ready(function () {
     });
 
     //animatedModal
-    $("#demo01,#demo02,#demo03,#demo04,#demo05,#demo06,#demo07,#demo08,#demo09").animatedModal();
+    // $("#demo01,#demo02,#demo03,#demo04,#demo05,#demo06,#demo07,#demo08,#demo09").animatedModal();
     // $("#demo01").animatedModal1();
     
     // Contact Form 	
