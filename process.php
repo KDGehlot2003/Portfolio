8356989859fcc4e24c8fe65be47b9bfd91a02ed3
Kshitij Gehlot <kdgehlot2003@gmail.com>
2024-05-28 15:12:27 +0530

    initial commit

---
+++ process.php
@@ -0,0 +1,24 @@
+<?php
+
+    $to = "kdgehlot2003@gmail.com";
+    $from = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+    $headers = "From: $from";
+    $subject = "You have a message.";
+
+    $fields = array();
+    $fields{"name"} = "name";
+    $fields{"email"} = "email";
+    $fields{"phone"} = "phone";
+    $fields{"message"} = "message";
+
+    
+
+    $body = "Here is what was sent:\r\n"; 
+
+    foreach($fields as $a => $b){$body .= $b." : ".$_REQUEST[$a]."\r\n"; }
+
+
+    $send = mail($to, $subject, $body, $headers);
+
+?>
